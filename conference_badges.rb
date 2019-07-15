a03c4312301d7dc821e2c0dd11de68880935845d
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-15 19:40:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+# speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def batch_badge_creator(speakers)
   
 end 
