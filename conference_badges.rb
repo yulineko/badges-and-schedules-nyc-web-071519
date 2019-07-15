fd20fb8f06253cdf38865dfc2eed53183080314c
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-15 19:45:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,8 @@ def batch_badge_creator(speakers)
   end
 end 
 
-def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+def assign_rooms(speakers)
+  speakers.each_with_index.map do |speaker, i|
+    "Hello, #{speaker} You'll be assigned to room #{i + 1}!"
   end
 end
